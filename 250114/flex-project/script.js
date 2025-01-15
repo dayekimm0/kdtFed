1375a8cce4ce19389e0e2ca10909db1250b0d5d9
dayekimm0 <dayekimm0@gmail.com>
2025-01-15 19:06:48 +0900

    250115

--- 250114/flex-project/script.js
+++ 250114/flex-project/script.js
@@ -5,6 +5,9 @@
 // 2-1. header는 html안에서 header라는 태그를 사용하고 있는 요소라고 알리기
 // 2-2. 사용자가 스크롤을 내렸으니 빨리 액티브 클래스를 header에게 적용
 //
+// 브라우저가 가지고 있는 내장객체
+// document.querySelector("#circle");
+//문서 => 개발자가 직접 작성한 html문서
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 50) {
@@ -13,6 +16,14 @@ window.addEventListener("scroll", () => {
     document.querySelector("header").classList.remove("active");
   }
 });
-// 브라우저가 가지고 있는 내장객체
-// document.querySelector("#circle");
-//문서 => 개발자가 직접 작성한 html문서
+
+/* slick slider */
+$(document).ready(function () {
+  $(".myslider").slick({
+    slidesToShow: 3,
+    slidesToScorll: 3,
+    infinite: false,
+    dots: true,
+    speed: 300,
+  });
+});
