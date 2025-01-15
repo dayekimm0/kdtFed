bd069e9a6c19749793d482047788a89007896779
dayekimm0 <dayekimm0@gmail.com>
2025-01-15 10:38:28 +0900

    250115

---
+++ 250114/flex-project/script.js
@@ -0,0 +1,18 @@
+// 목표 : 사용자가 스크롤을 내렸을 때, header 배경컬러 // banner height를 변경 => 가상 클래스 적용
+// 1. 사용자가 지금 스크롤을 내렸어
+// > 스크롤 - 브라우저가 가지고 있는 요소
+// > 브라우저 지금 스크롤이 내려갔니
+// 2-1. header는 html안에서 header라는 태그를 사용하고 있는 요소라고 알리기
+// 2-2. 사용자가 스크롤을 내렸으니 빨리 액티브 클래스를 header에게 적용
+//
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    document.querySelector("header").classList.add("active");
+  } else {
+    document.querySelector("header").classList.remove("active");
+  }
+});
+// 브라우저가 가지고 있는 내장객체
+// document.querySelector("#circle");
+//문서 => 개발자가 직접 작성한 html문서
