19e4cb3bf2f189c02fad08b2bccf5b8a215a2794
dayekimm0 <dayekimm0@gmail.com>
2025-01-23 13:14:16 +0900

    250123

--- 250123/script02.js
+++ 250123/script02.js
@@ -8,11 +8,23 @@ console.log(fruits);
 // console.log(...fruits);
 */
 
-const addNum = (...numbers) => {
-  let sum = 0;
-  numbers.forEach((number) => {
-    sum += number;
-  });
-  return sum;
+// const addNum = (...numbers) => {
+//   let sum = 0;
+//   /*
+//   numbers.forEach((number) => {
+//     sum += number;
+//   });
+//   */
+
+//   for (let number of numbers) {
+//     sum += number;
+//   }
+//   return sum;
+// };
+// console.log(addNum(1, 3, 5, 7));
+
+const displayFavorites = (first, ...fruits) => {
+  const str = `내가 가장 좋아하는 과일은 ${first}입니다.`;
+  return str;
 };
-console.log(addNum(1, 3, 5, 7));
+console.log(displayFavorites("사과", "포도", "토마토"));
