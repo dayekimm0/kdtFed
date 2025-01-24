6e493caaff19d2bdc6798aa67a8651751b2ffa5b
dayekimm0 <dayekimm0@gmail.com>
2025-01-24 12:53:44 +0900

    250124

---
+++ 250123/script03.js
@@ -0,0 +1,19 @@
+// const hello = () => {
+//   console.log("Hello World!");
+// };
+
+// setInterval(hello, 3000);
+
+// let counter = 0;
+
+// let timer = setInterval(() => {
+//   console.log("Hello World!");
+//   counter++;
+//   if (counter === 5) {
+//     clearInterval(timer);
+//   }
+// }, 2000);
+
+setTimeout(() => {
+  console.log("안녕하세요!");
+}, 3000);
