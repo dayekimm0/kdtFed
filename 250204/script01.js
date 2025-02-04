8e69e0205ac751eab7a65cb00eee7c0656844282
dayekimm0 <dayekimm0@gmail.com>
2025-02-04 10:45:14 +0900

    250204

---
+++ 250204/script01.js
@@ -0,0 +1,6 @@
+document.querySelector("#userId").addEventListener("focus", function () {
+  this.style.backgroundColor = "pink";
+});
+document.querySelector("#userId").addEventListener("blur", function () {
+  this.style.backgroundColor = "transparent";
+});
