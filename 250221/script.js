aa07a56c7f9f169a7fef0424e19c0e726a558f5f
dayekimm0 <dayekimm0@gmail.com>
2025-02-21 10:20:38 +0900

    250221

---
+++ 250221/script.js
@@ -0,0 +1,5 @@
+const input = document.querySelector(".number-input");
+const min = input.getAttribute("data-min");
+const max = input.getAttribute("data-max");
+const step = input.getAttribute("data-step");
+const value = input.getAttribute("data-value");
