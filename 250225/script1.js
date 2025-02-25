402a3a211b1c7965fb0d62fb81446098d0150a47
dayekimm0 <dayekimm0@gmail.com>
2025-02-25 20:06:01 +0900

    250225

---
+++ 250225/script1.js
@@ -0,0 +1,10 @@
+const fruits = ["사과", "복숭아"];
+// const apple = fruits[0];
+// const peach = fruits[1];
+const [apple, peach] = fruits;
+console.log(apple, peach);
+
+const seasons = ["봄", "여름", "가을", "겨울"];
+const [spring, summer] = seasons;
+
+console.log(spring, summer);
