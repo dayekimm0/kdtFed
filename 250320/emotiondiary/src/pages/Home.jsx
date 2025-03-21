c644dacd21dc512e884a58c25695c13838667ede
dayekimm0 <dayekimm0@gmail.com>
2025-03-21 09:28:22 +0900

    250321

---
+++ 250320/emotiondiary/src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Button from "../components/Button";
+import Header from "../components/Header";
+import Editor from "../components/Editor";
+
+const Home = () => {
+  return (
+    <div>
+      <Header
+        title="Home"
+        leftChild={
+          <Button
+            text="긍정버튼"
+            type="positive"
+            onClick={() => {
+              alert("긍정");
+            }}
+          />
+        }
+        rightChild={
+          <Button
+            text="부정버튼"
+            type="negative"
+            onClick={() => {
+              alert("부정");
+            }}
+          />
+        }
+      />
+      <Editor />
+    </div>
+  );
+};
+
+export default Home;
