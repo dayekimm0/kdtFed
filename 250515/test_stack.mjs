b3ef04fd5d233e387214da54953a561fedd4e332
dayekimm0 <dayekimm0@gmail.com>
2025-05-15 20:00:01 +0900

    250515

---
+++ 250515/test_stack.mjs
@@ -0,0 +1,27 @@
+import { LinkedList } from "./linkedList.mjs";
+
+class stack {
+  constructor() {
+    this.list = new LinkedList();
+  }
+
+  push(data) {
+    this.list.insertAt(0, data);
+  }
+
+  pop() {
+    try {
+      return this.list.deleteAt(0);
+    } catch (e) {
+      return null;
+    }
+  }
+
+  peek() {
+    return this.list.getNodeAt(0);
+  }
+
+  isEmpty() {
+    return this.list.count === 0;
+  }
+}
